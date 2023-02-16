1153adbfbcdeae4367a2c6a396a2a71066910dc9
deyja8 <72979041+deyja8@users.noreply.github.com>
2023-02-16 15:23:03 +0100

    1st update

---
+++ app/src/components/Email/Email.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Input } from "../Input";
+
+import "./style.css";
+
+export const Email = () => {
+  return (
+    <div className="e-input">
+      <h3 className="font">Email</h3>
+      <Input />
+    </div>
+  );
+};
---
+++ app/src/components/index.js
@@ -0,0 +1,7 @@
+export * from "./SignIn";
+export * from "./SignUp";
+export * from "./Input";
+export * from "./Name";
+export * from "./Email";
+export * from "./Password";
+export * from "./Buttons";
